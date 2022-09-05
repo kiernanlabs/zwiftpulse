56ce5ed355acdd9d65c97382e8300bcb1afa50cc
Joe Kiernan <joe@kiernanlabs.com>
2022-09-04 21:12:11 -0400

    top 5 races only

--- racereport/views.py
+++ racereport/views.py
@@ -11,7 +11,7 @@ from django.views.generic import (
 from .models import Race, RaceCat, RaceResult, Team
 
 def index(request):
-    race_cats = RaceCat.objects.racecats_last24hrs().annotate(racer_count=Count('raceresult')).order_by("-racer_count")
+    race_cats = RaceCat.objects.racecats_last24hrs().annotate(racer_count=Count('raceresult')).order_by("-racer_count")[:5]
     top_teams = Team.objects.get_top_10_teams()
     last_race_imported = Race.objects.last()
     most_recent_race_imported = Race.objects.latest('event_datetime')
